5e0e67301dd9b76916cf0e290d797e8d9d852dbe
Vladimir Izmalkov <vladimir@vaticle.com>
2024-02-14 14:52:55 +0000

    Update rule-api section

--- main.py
+++ main.py
@@ -184,7 +184,6 @@ with TypeDB.core_driver("localhost:1729") as driver:
             for subtype in subtypes:
                 print(subtype.get_label().name)
             transaction.commit()
-
     # end::types-api[]
     # tag::rules-api[]
     with driver.session(DB_NAME, SessionType.SCHEMA) as session:
@@ -197,18 +196,7 @@ with TypeDB.core_driver("localhost:1729") as driver:
             new_rule = transaction.logic.put_rule("Employee",
                                                   "{$u isa user, has email $e; $e contains '@vaticle.com';}",
                                                   "$u has name 'Employee'").resolve()
-            print("Rules (before deletion):")
-            rules = transaction.logic.get_rules()
-            for rule in rules:
-                print("Rule label:", rule.label)
-
+            print(transaction.logic.get_rule("Employee").resolve().label)
             new_rule.delete(transaction).resolve()
-
-            print("Rules (after deletion):")
-            rules = transaction.logic.get_rules()
-            for rule in rules:
-                print("Rule label:", rule.label)
-
             transaction.commit()
-
     # end::rules-api[]
